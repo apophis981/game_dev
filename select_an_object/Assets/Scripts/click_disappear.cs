b9e344c2917c09f1ac62e51c41777d5f5b632b06
Jeremy Hurst <jeremy_hurst@cable.comcast.com>
2015-02-22 18:27:12 -0500

    Adding select_an_object framework

---
+++ select_an_object/Assets/Scripts/click_disappear.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class click_disappear : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
